2196aa506c66526ed571184d2ffef973d05e07a9
Paresh <paresh_mangukiya@jisto.com>
2018-10-19 14:54:09 +0530

    First commit

---
+++ client/components/SongList.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+import  query from '../queries/fetchSongs';
+
+class SongList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteSong = this.deleteSong.bind(this);
+  }
+
+  deleteSong(id) {
+    this.props.mutate({
+      variables: {
+        id
+      }
+    }).then(() => {
+      return this.props.data.refetch();
+    });
+  };
+
+  renderSongs() {
+    return this.props.data && this.props.data.songs && this.props.data.songs.map(({ id, title }) => {
+      return (
+        <li key={id} className='collection-item'>
+          { title }
+          <button className='btn-floatin btn-small red right' onClick={() => this.deleteSong(id)}>Delete</button>
+        </li>
+      );
+    })
+  }
+
+  render() {
+    if (this.props.data && this.props.data.loading) {
+      return (
+        <div>Loading... </div>
+      );
+    }
+    return (
+      <div>
+        <ul className='collection'>
+        {
+          this.renderSongs()
+        }
+        </ul>
+        <Link to='/songs/new' className='btn-floating btn-large blue right'>
+          Add
+        </Link>
+    </div>
+  )}
+}
+
+const delMutation = gql`
+  mutation DeleteSong($id:ID) {
+    deleteSong(id: $id) {
+      id
+    }
+}`;
+
+
+export default graphql(delMutation)(
+  graphql(query)(SongList));
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://vrunda:vrunda5@ds111489.mlab.com:11489/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
